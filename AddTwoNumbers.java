2ab033e2731925be322506dda08fa14f1406a770
SrishtiVel <51516831+SrishtiVel@users.noreply.github.com>
2024-05-28 18:53:02 -0400

    Update and rename Solution.java to AddTwoNumbers.java
    Specify the problem

--- AddTwoNumbers.java
+++ AddTwoNumbers.java
@@ -8,7 +8,7 @@
  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
-class Solution {
+class AddTwoNumbers {
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
         ListNode l1Head = l1;
         ListNode l2Head = l2;
